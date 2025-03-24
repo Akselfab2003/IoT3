c93e507a0fdb98d62eb8951c6ca179896e5e325a
Aksel Fabricius <akselfabricius@gmail.com>
2025-03-24 09:11:04 +0100

    Implement sensor reading functionality and add Visual Studio Code configuration

--- ESP32_Project/IOT3/src/main.cpp
+++ ESP32_Project/IOT3/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+#define SENSOR_PIN 18
+
+void ReadSensor();
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(9600);
+  pinMode(SENSOR_PIN, INPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  ReadSensor();
+  delay(1000);
+
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
+}
+
+
+
+void ReadSensor(){
+  int state = digitalRead(SENSOR_PIN);
+  if (state == LOW)
+  {
+    Serial.println("Object Detected");
+  }
+  else
+  {
+    Serial.println("No Object Detected");
+  }
 }
