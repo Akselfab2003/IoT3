b950384f68d37d5b10dac6bb5b72c2438eb3b417
MR-Vall <markrobbers3@gmail.com>
2025-03-25 10:16:56 +0100

    Refactor PeopleCount loop logic for improved readability and state handling

--- ESP32_Project/IOT3/src/PeopleCount.cpp
+++ ESP32_Project/IOT3/src/PeopleCount.cpp
@@ -26,37 +26,40 @@ void setup1() {
 
 void loop1() {
   Serial.println("Loopi loop");
-  int stateA = digitalRead(SENSOR_PIN_Lose);
-  int stateB = digitalRead(SENSOR_PIN_Board);
+  int stateLose = digitalRead(SENSOR_PIN_Lose);
+  int stateBoard = digitalRead(SENSOR_PIN_Board);
   unsigned long currentTime = millis();
 
-  Serial.println("State A =" + stateA);
-  Serial.println("State B =" + stateB);
-  Serial.println("Current Time =" + currentTime);
+  Serial.println("State Lose =" + String(stateLose));
+  Serial.println("State Board =" + String(stateBoard));
+  Serial.println("Current Time =" + String(currentTime));
 
   // Reset the state if the time window has expired
   if (currentState != WAITING && (currentTime - triggerTime > timeWindow)) {
     currentState = WAITING;
   }
 
-  Serial.println("Current State: " + currentState);
+  
   if (currentState == WAITING) {
-    if (stateA == LOW) {
+    if (stateLose == LOW) {
       // Outside sensor triggered first
       currentState = SENSOR_A_TRIGGERED;
       triggerTime = currentTime;
       delay(50);  // Simple debounce delay
     }
-    else if (stateB == LOW) {
+    else if (stateBoard == LOW) {
       // Inside sensor triggered first
       currentState = SENSOR_B_TRIGGERED;
       triggerTime = currentTime;
       delay(50);
     }
+    Serial.println("Current State: " + String(currentState));
   }
   else if (currentState == SENSOR_A_TRIGGERED) {
+    
+    Serial.println("Current State SENSOR A TRIGGER: " + String(currentState));
     // Waiting for the inside sensor to trigger to confirm an entry
-    if (stateB == LOW) {
+    if (stateBoard == LOW) {
       // Sequence: sensorA then sensorB → Person entering
       peopleCount++;
       Serial.print("Person entered. Count: ");
@@ -67,7 +70,10 @@ void loop1() {
   }
   else if (currentState == SENSOR_B_TRIGGERED) {
     // Waiting for the outside sensor to trigger to confirm an exit
-    if (stateA == LOW) {
+
+    Serial.println("Current State SENSOR B TRIGGER: " + String(currentState));
+
+    if (stateLose == LOW) {
       // Sequence: sensorB then sensorA → Person exiting
       peopleCount--;
       if (peopleCount < 0) peopleCount = 0;
