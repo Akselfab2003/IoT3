440b59b13a9f8ebf7857d97cfdf91a5e521cf08e
Aksel Fabricius <akselfabricius@gmail.com>
2025-03-26 08:01:20 +0100

    Refactor WiFi configuration: create separate header and implementation files; improve connection feedback

--- ESP32_Project/IOT3/src/wifi_configuration.cpp
+++ ESP32_Project/IOT3/src/wifi_configuration.cpp
@@ -1,12 +1,11 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <wifi_configuration.h>
 
 const char* ssid = "your_ssid";
 const char* password = "your_password";
 
 const char* mqtt_server = "your_mqtt_server";
-
-
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -14,46 +13,19 @@ long lastMsg = 0;
 char msg[50];
 int value = 0;
 
+void initialize_WiFi(){
 
-void callback(char* topic, byte* payload, unsigned int length) {
-    Serial.print("Message arrived [");
-    Serial.print(topic);
-    Serial.print("] ");
-    for (int i = 0; i < length; i++) {
-        Serial.print((char)payload[i]);
-    }
-    Serial.println();
-}
+    Serial.println("Attempting to connect to WiFi network");
+    Serial.println("SSID: " + String(ssid));
+    WiFi.begin(ssid,password);
 
-
-void SetupWifiConfiguration(){
-    setup_wifi();
-    client.setServer(mqtt_server, 1883);
-    client.setCallback(callback);
-    Serial.println("Wifi Configuration Setup Done");
-}
-
-
-
-void setup_wifi(){
-    delay(10);
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-
-    WiFi.begin(ssid, password);
-
-
-    while (WiFi.status() != WL_CONNECTED)
-    {
+    while(WiFi.status() != WL_CONNECTED){
         delay(500);
         Serial.print(".");
     }
 
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    
+    if (WiFi.status() == WL_CONNECTED){
+        Serial.println("Connected to WiFi network");
+        Serial.println("IP Address: " + WiFi.localIP().toString());
+    }
 }
---
+++ ESP32_Project/IOT3/src/wifi_configuration.h
@@ -0,0 +1,9 @@
+#ifndef WiFi_Configuration_h
+#define WiFi_Configuration_h
+#include <WiFi.h>
+
+extern WiFiClient espClient;
+
+void initialize_WiFi();
+
+#endif
