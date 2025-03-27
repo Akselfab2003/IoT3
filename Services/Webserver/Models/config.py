71059b0b907680f9a9246c2b2bad7be43b6e5785
Aksel Fabricius <akselfabricius@gmail.com>
2025-03-27 09:34:01 +0100

    Refactor PeopleCounter model and database handling; streamline session management and improve directory creation logic

--- Services/Webserver/Models/config.py
+++ Services/Webserver/Models/config.py
@@ -4,3 +4,4 @@ load_dotenv()
 class Config:
     def __init__(self):
         self.DB_PATH = os.getenv("DB_PATH")
+
---
+++ Services/Webserver/Models/db.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config import Config
+import os
+
+def Create_Database_Path_If_Needed(PATH=None):
+    db_file_path = PATH.replace("sqlite:///", "")  
+    db_dir = os.path.dirname(db_file_path)
+
+    if not os.path.exists(db_dir):
+        os.makedirs(db_dir)  # Create the directory if it does not exist
+
+DB_PATH = Config().DB_PATH
+
+Create_Database_Path_If_Needed(DB_PATH)
+
+DB_ENGINE = create_engine(DB_PATH)
+
+SESSION_MAKER = sessionmaker(bind=DB_ENGINE)
+
+
