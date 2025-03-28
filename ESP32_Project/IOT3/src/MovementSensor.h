4a24d6fd83cb1ca7974085471942a1ebc02e2c9e
Aksel Fabricius <akselfabricius@gmail.com>
2025-03-28 10:03:35 +0100

    Add id field to Move_Sensor struct for unique identification

--- ESP32_Project/IOT3/src/MovementSensor.h
+++ ESP32_Project/IOT3/src/MovementSensor.h
@@ -5,7 +5,7 @@
 struct Move_Sensor 
 {
     Move_Sensor(std::string name, std::string type,std::string unit, float value) : name(name), type(type), unit(unit), value(value) {}
-
+    int id = 0;
     std::string name;
     std::string type;
     std::string unit;
