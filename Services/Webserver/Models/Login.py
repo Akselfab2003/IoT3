45ccf5240104a9b429c16cd37c845615067190c1
Aksel Fabricius <akselfabricius@gmail.com>
2025-04-01 11:43:02 +0200

    Implement keycard hashing and verification in Login model for enhanced security

--- Services/Webserver/Models/Login.py
+++ Services/Webserver/Models/Login.py
@@ -1,6 +1,7 @@
 from .base import Base
 from sqlalchemy import Column, Integer, String
 from .db import get_session
+from argon2 import PasswordHasher
 
 class Login(Base):
     __tablename__ = 'logins'
@@ -12,7 +13,21 @@ class Login(Base):
 
     def __init__(self, username, keycard):
         self.username = username
-        self.keycard = keycard
+        self.keycard = self.hash_keycard(keycard)
+    
+    @staticmethod
+    def hash_keycard(keycard):
+        ph = PasswordHasher()
+        return ph.hash(keycard)
+    
+    @staticmethod
+    def verify_keycard(hashed_keycard, keycard):
+        ph = PasswordHasher()
+        try:
+            return ph.verify(hashed_keycard, keycard)
+        except Exception as e:
+            print(f"Error verifying keycard: {e}")
+            return False
 
 def add_new_login(login:Login):
     with get_session() as session:
@@ -21,6 +36,14 @@ def add_new_login(login:Login):
 
 def login(login:Login):
     with get_session() as session:
-        logins = session.query(Login).filter(Login.username == login.username).filter(Login.keycard == login.keycard).all()
-        return len(logins) > 0
-    
+        try:
+            logins = session.query(Login).filter(Login.username == login.username).first()
+            if logins is None:
+                return False
+            if Login.verify_keycard(logins.keycard, login.keycard):
+                return True
+            
+            return False
+        except:
+            return False
+        
